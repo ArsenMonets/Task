d190859a5829c3192260e74402aa32fc96aff776
ArsenMonets <146364050+ArsenMonets@users.noreply.github.com>
2023-09-29 12:42:50 +0300

    Java program with arrays added

---
+++ ShopWithArray.java
@@ -0,0 +1,9 @@
+class Shop {
+   public static void main(String[] args) {
+      String[] productName = {"Milk", "Bread", "Yogurt"};
+      int[] price = {2, 1, 3};
+      for (int i = 0; i<3; i++) {
+      System. out.println(productName[i] + ": " + price[i] + "$");
+      }
+   }
+}
