85aa8c8ddb362d397ef09baa3ce098ad9f236eb8
ArsenMonets <146364050+ArsenMonets@users.noreply.github.com>
2023-09-29 12:41:55 +0300

    C# program added

---
+++ Shop.cs
@@ -0,0 +1,9 @@
+using System;
+
+  public class Shop {
+     public static void Main(string[] args) {
+       string productName = "Milk";
+       int price = 2;
+       Console.WriteLine (productName + ": " + price + "$");
+  }
+     }
